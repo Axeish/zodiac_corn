70ab72552459804bd786f2e044fc7f672c7e46ee
ashish kumar <axeishkr@gmail.com>
2023-11-20 14:38:39 -0600

    first commit

--- zodiac_corn/ContentView.swift
+++ zodiac_corn/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello to my works, checking git setup world!")
         }
         .padding()
     }
