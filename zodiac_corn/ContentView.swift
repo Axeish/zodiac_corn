4e76851f91a6ec8cb892f62eab672b403181991d
ashish kumar <axeishkr@gmail.com>
2023-11-20 14:31:11 -0600

    Initial Commit

---
+++ zodiac_corn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  zodiac_corn
+//
+//  Created by Ashish Kumar on 11/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ zodiac_corn/zodiac_cornApp.swift
@@ -0,0 +1,17 @@
+//
+//  zodiac_cornApp.swift
+//  zodiac_corn
+//
+//  Created by Ashish Kumar on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct zodiac_cornApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
